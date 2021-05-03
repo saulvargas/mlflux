e35e465df2a88abbc12b5bad2cd7dbdcd6e64230
Saúl Vargas <saul@infogrid.io>
2021-05-03 19:07:07 +0300

    store output of run as artifacts

--- mlflux/cli/run.py
+++ mlflux/cli/run.py
@@ -63,10 +63,9 @@ def _copy_from_uri(uri: str, project_path: Path, output_path) -> Optional[Path]:
 
     shutil.copytree(src_path, project_path, ignore=ignore, dirs_exist_ok=True)
 
-    base_path = output_path / "src"
-    src_zip_path = Path(shutil.make_archive(base_path, "zip", project_path))
+    src_zip = Path(shutil.make_archive(output_path / "src", "zip", project_path))
 
-    return src_zip_path
+    return src_zip
 
 
 def _setup_docker_image(project_path: Path, output_path: Path) -> Optional[Path]:
@@ -108,11 +107,30 @@ def _setup_docker_image(project_path: Path, output_path: Path) -> Optional[Path]
         return docker_logs_path
 
 
-def _store_docker_logs(docker_logs, output_path):
-    docker_logs_path = output_path / "docker.stdout.txt"
-    with docker_logs_path.open("w") as f:
-        for log in docker_logs:
-            f.write(log)
+def _setup_entrypoint_output(project_path, entry_point, output_path):
+    ml_project_path = project_path / "MLproject"
+
+    with ml_project_path.open() as f:
+        ml_project = yaml.safe_load(f)
+
+    volumes = ml_project["docker_env"].get("volumes", [])
+    volumes.append(f"{output_path.as_posix()}:/output")
+    ml_project["docker_env"]["volumes"] = volumes
+
+    stdout = output_path / "stdout.txt"
+    stderr = output_path / "stderr.txt"
+
+    command = ml_project["entry_points"][entry_point]["command"]
+    wrapper = project_path / "wrapper.sh"
+    wrapper.write_text(
+        f"{command} $@ 1> >(tee /output/stdout.txt) 2> >(tee /output/stderr.txt >&2)"
+    )
+    ml_project["entry_points"][entry_point]["command"] = f"bash wrapper.sh"
+
+    with ml_project_path.open("w") as f:
+        yaml.safe_dump(ml_project, f)
+
+    return stdout, stderr
 
 
 def run(
@@ -127,8 +145,12 @@ def run(
         project_path = Path(project_dir)
         output_path = Path(output_dir)
 
-        src_zip_path = _copy_from_uri(uri, project_path, output_path)
-        docker_logs_path = _setup_docker_image(project_path, output_path)
+        src = _copy_from_uri(uri, project_path, output_path)
+        docker_logs = _setup_docker_image(project_path, output_path)
+
+        stdout, stderr = _setup_entrypoint_output(
+            project_path, entry_point, output_path
+        )
 
         mlflow_run = mlflow.projects.run(
             uri=project_path.as_posix(),
@@ -137,5 +159,7 @@ def run(
         )
 
         with mlflow.start_run(mlflow_run.run_id):
-            mlflow.log_artifact(src_zip_path.as_posix())
-            mlflow.log_artifact(docker_logs_path.as_posix(), "logs")
+            mlflow.log_artifact(src.as_posix())
+            mlflow.log_artifact(docker_logs.as_posix(), "logs")
+            mlflow.log_artifact(stdout.as_posix(), "logs")
+            mlflow.log_artifact(stderr.as_posix(), "logs")
